9a8f29305fc568981e5d8d7b7b04d2e833a4c547
Pradeepika <pradeepika.junapudi@vearc.com>
2022-07-05 13:45:16 +0530

    added models - airline, alliance, airport

--- air/models.py
+++ air/models.py
@@ -10,8 +10,8 @@ class Alliance(models.Model):
 
 
 class Airport(models.Model):
-    airport_id = models.IntegerField()
     airport_code = models.CharField(max_length=6, primary_key=True)
+    airport_id = models.IntegerField()
     airport_city = models.CharField(max_length=100)
 
     def __str__(self):
@@ -20,7 +20,7 @@ class Airport(models.Model):
 class Airline(models.Model):
     carrier_id = models.IntegerField()
     carrier_name = models.CharField(max_length=100)
-    carrier_code = models.CharField(max_length=4, primary_key=True, unique=True)
+    carrier_code = models.CharField(max_length=4, primary_key=True)
     alliance_id = models.ForeignKey("Alliance", verbose_name="alliance_id", on_delete=models.CASCADE)
 
     def __str__(self):
